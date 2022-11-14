ab4aadc0757e61e7390c4d01ca61f3cfcc232a84
Kevin Constantin <kconstantin@protonmail.com>
2022-11-14 09:53:54 -0800

    notes exercises

---
+++ part2/notes/src/components/App.js
@@ -0,0 +1,53 @@
+import Note from './Note';
+import { useState } from 'react';
+
+export default function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll ? 
+    notes : notes.filter(note => note.important);
+
+  const addNote = (event) => {
+    event.preventDefault();
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'Important' : 'All'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form> 
+    </div>
+  )
+}
